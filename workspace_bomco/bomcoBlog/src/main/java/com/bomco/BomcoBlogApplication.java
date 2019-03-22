ec7a9d39a887f2a0721984331a8bd3787223f328
봄코딩 <bomcoding@gmail.com>
2019-03-22 12:39:30 +0900

    bomco_blog Framwork : Spring Boot

---
+++ workspace_bomco/bomcoBlog/src/main/java/com/bomco/BomcoBlogApplication.java
@@ -0,0 +1,13 @@
+package com.bomco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BomcoBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BomcoBlogApplication.class, args);
+	}
+
+}
